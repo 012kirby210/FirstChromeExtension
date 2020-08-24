93e9bab35918be430cb85a34acddf53ab90f01d5
012kirby210 <012kirby210@gmail.com>
2020-08-24 22:15:55 +0200

    some icons and business layer

--- background.js
+++ background.js
@@ -2,6 +2,26 @@
 
 chrome.runtime.onInstalled.addListener(function(){
   chrome.storage.sync.set({color: '#3aa757'}, function(){
-    console.log("The color is green");
+    console.log("The color key has been set.");
+  });
+
+  let pageActionRule =
+  {
+    conditions: [
+      new chrome.declarativeContent.PageStateMatcher(
+        {
+          pageUrl: {
+            hostEquals: 'developer.chrome.com',
+            schemes: ['https']
+          }
+        }
+      )
+    ],
+    actions: [
+      new chrome.declarativeContent.ShowPageAction()
+    ]
+  };
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
+    chrome.declarativeContent.onPageChanged.addRules([pageActionRule]);
   });
 });
